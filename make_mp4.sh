6c5b39907e6254086efc68914b34637d7c8afae8
Nicholas LaRoche <nicholas.louis.laroche@outlook.com>
2026-02-15 18:25:27 -0700

    Updated config for make_mp4.sh

--- make_mp4.sh
+++ make_mp4.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 python3 scripts/enciphered_bins_video.py \
-  --input enciphered_decryption_bins.csv \
-  --output enciphered_bins.mp4 \
+  --input enciphered_decryption_bins2.csv \
+  --output enciphered_bins-2.mp4 \
   --metric float \
+  --azim-step 1 \
   --parallel \
   --z-scale linear \
-  --z-max 75 \
-  --z-min 40 \
+  --z-max 80 \
+  --z-min 20 \
+  --max-frames 600 \
   --smooth-window 1
